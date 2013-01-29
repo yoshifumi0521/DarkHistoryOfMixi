2c1752566e44207088f04f232e3158700d3707df
YoshifumiSato <yoshifumi0521@gmail.com>
2013-01-29 19:34:52 +0900

    railsアプリを作成した

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DarkHistoryOfMixi::Application.config.secret_token = '2916faf4a2eec557445dff9f8cc9c1a1cf9c901a1cbb548394fb8c189e6447fa9932691b160fdf8a18ba4384886b0881f5f2c158b342550d8af772da54464daa'
